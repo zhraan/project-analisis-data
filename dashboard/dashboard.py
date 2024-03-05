b219839b19bf2c336093ffa48cb07b14e84731e5
zahran523 <zahranfikri523@gmail.com>
2024-03-05 14:03:56 +0700

    background dataset

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -40,7 +40,16 @@ with st.sidebar:
     
     st.header("Data Analysis Project")
     st.subheader("By Zahran Fikri")
-    # st.image("")
+    # st.image("https://github.com/zhraan/project-analisis-data/blob/main/sepeda.jpg")
+    st.caption('')
+    with st.expander("Dataset Background"):
+        st.write("""Bike sharing systems are new generation of traditional bike rentals where whole process from membership, rental and return 
+            back has become automatic. Through these systems, user is able to easily rent a bike from a particular position and return 
+            back at another position. Currently, there are about over 500 bike-sharing programs around the world which is composed of 
+            over 500 thousands bicycles. Today, there exists great interest in these systems due to their important role in traffic, 
+            environmental and health issues. 
+            """
+        )
 
 st.header("Bike Sharing Data Dashboard :sparkles:")
 
